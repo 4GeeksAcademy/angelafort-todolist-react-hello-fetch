3e3a032a0bc9bda2165370763bf4213b04f55cf1
angelafort <139291628+angelafort@users.noreply.github.com>
2024-04-12 23:46:26 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+	const [hoveredIndex, setHoveredIndex] = useState(null);
+  
+	const handleMouseEnter = (index) => {
+	  setHoveredIndex(index);
+	};
+  
+	const handleMouseLeave = () => {
+	  setHoveredIndex(null);
+	};
+  
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	  <div className="container">
+		<h1>todos</h1>
+		<ul>
+		  <li>
+			<input
+			  type="text"
+			  onChange={(e) => setInputValue(e.target.value)}
+			  value={inputValue}
+			  onKeyPress={(e) => {
+				if (e.key === "Enter") {
+				  setTodos(todos.concat([inputValue]));
+				  setInputValue("");
+				}
+			  }}
+			  placeholder="What needs to be done?"
+			></input>
+		  </li>
+		  {todos.length === 0 ? (
+			<li>No tasks, add a task</li>
+		  ) : (
+			todos.map((item, index) => (
+			  <li
+				key={index}
+				onMouseEnter={() => handleMouseEnter(index)}
+				onMouseLeave={handleMouseLeave}
+			  ><div className="task-list-item">
+				{item}{" "}
+				{hoveredIndex === index && (
+			   <div className= "icon"> <FontAwesomeIcon
+					icon={faTrash}
+					onClick={() =>
+					  setTodos(
+						todos.filter((t, currentIndex) => index !== currentIndex)
+					  )
+					}
+				  /></div>
+				)}</div>
+			  </li>
+			))
+		  )}
+		</ul>
+		<div>{todos.length} tasks</div>
+	  </div>
 	);
-};
+  };
 
 export default Home;
