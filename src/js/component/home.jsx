242a77fde8d596e56eaac400f79af56e6342db84
angelafort <139291628+angelafort@users.noreply.github.com>
2024-04-22 15:00:21 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,69 +1,108 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	const [inputValue, setInputValue] = useState("");
-	const [todos, setTodos] = useState([]);
-	const [hoveredIndex, setHoveredIndex] = useState(null);
+    const todosURL = "https://playground.4geeks.com/todo/";
+    const [inputValue, setInputValue] = useState("");
+    const [todos, setTodos] = useState([]);
+    const [hoveredIndex, setHoveredIndex] = useState(null);
+
+    const handleMouseEnter = (index) => {
+        setHoveredIndex(index);
+    };
   
-	const handleMouseEnter = (index) => {
-	  setHoveredIndex(index);
-	};
-  
-	const handleMouseLeave = () => {
-	  setHoveredIndex(null);
-	};
-  
-	return (
-	  <div className="container">
-		<h1>todos</h1>
-		<ul>
-		  <li>
-			<input
-			  type="text"
-			  onChange={(e) => setInputValue(e.target.value)}
-			  value={inputValue}
-			  onKeyPress={(e) => {
-				if (e.key === "Enter") {
-				  setTodos(todos.concat([inputValue]));
-				  setInputValue("");
-				}
-			  }}
-			  placeholder="What needs to be done?"
-			></input>
-		  </li>
-		  {todos.length === 0 ? (
-			<li>No tasks, add a task</li>
-		  ) : (
-			todos.map((item, index) => (
-			  <li
-				key={index}
-				onMouseEnter={() => handleMouseEnter(index)}
-				onMouseLeave={handleMouseLeave}
-			  ><div className="task-list-item">
-				{item}{" "}
-				{hoveredIndex === index && (
-			   <div className= "icon"> <FontAwesomeIcon
-					icon={faTrash}
-					onClick={() =>
-					  setTodos(
-						todos.filter((t, currentIndex) => index !== currentIndex)
-					  )
-					}
-				  /></div>
-				)}</div>
-			  </li>
-			))
-		  )}
-		</ul>
-		<div>{todos.length} tasks</div>
-	  </div>
-	);
-  };
+    const handleMouseLeave = () => {
+        setHoveredIndex(null);
+    };
+
+    useEffect(() => {
+        fetch(todosURL + "users/angelafort")
+            .then(response => response.json())
+            .then(data => {
+                setTodos(data.todos);
+            })
+            .catch(error => console.error(error));
+    }, []);
+
+    function deleteTodo(todoId) {
+        fetch(todosURL + "todos/" + todoId,  {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            setTodos(prevTodos => prevTodos.filter(todo => todo.id !== todoId));
+        })
+        .catch(error => console.error(error));
+    }
+
+    function addTodo() {
+        const newTodo = {
+            "label": inputValue
+        };
+
+        fetch(todosURL + "todos/angelafort", {
+            method: "POST",
+            body: JSON.stringify(newTodo),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            setTodos([...todos, data]);
+            setInputValue("");
+        })
+        .catch(error => console.error(error));
+    }
+
+    return (
+        <div className="container">
+            <h1>todos</h1>
+            <ul>
+                <li>
+                    <input
+                        type="text"
+                        onChange={(e) => setInputValue(e.target.value)}
+                        value={inputValue}
+                        onKeyPress={(e) => {
+                            if (e.key === "Enter") {
+                                addTodo();
+                            }
+                        }}
+                        placeholder="What needs to be done?"
+                    />
+                </li>
+                {todos.length === 0 ? (
+                    <li>No tasks, add a task</li>
+                ) : (
+                    todos.map((item, index) => (
+                        <li
+                            key={index}
+                            onMouseEnter={() => handleMouseEnter(index)}
+                            onMouseLeave={handleMouseLeave}
+                        >
+                            <div className="task-list-item">
+                                {item.label}{" "}
+                                {hoveredIndex === index && (
+                                    <div className= "icon">
+                                        <FontAwesomeIcon
+                                            icon={faTrash}
+                                            onClick={() => deleteTodo(item.id)}
+                                        />
+                                    </div>
+                                )}
+                            </div>
+                        </li>
+                    ))
+                )}
+            </ul>
+            <div>{todos.length} tasks</div>
+        </div>
+    );
+};
 
 export default Home;
